fc4f8c9a191aeca8f0d3c5c35d4e725d714ba007
b18030 <tushar22.tg.tg@gmail.com>
2020-10-13 15:20:54 +0530

    add exit message

--- PartA/sqrt.c
+++ PartA/sqrt.c
@@ -15,6 +15,7 @@ int main(int argc, char* argv[]) {
 
 	int input = atoi(argv[1]);
 	printf("Sqrt of %d is %f\n", input, sqrt(input));
+	printf("Added exit message\n");
 	return (0);
 
 } // end main
